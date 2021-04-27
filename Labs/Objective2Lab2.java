40c54ebe29a5ea90d1ff74ba7d39fabf8080cd77
Gabriel Avila <gabriel.avila.ga09@gmail.com>
2021-04-26 22:03:17 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2{
+  public static void main(String[] args){
+    String name = "Gabriel Avila";
+    int age = 25;
+    String state = "Texas";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
